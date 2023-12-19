9bdf16956c446c51d0e71942f59718edba2ce6c1
nsikakgoldman <georgegoldman.john.o.245299@unn.edu.ng>
2023-12-19 18:04:54 +0100

    user screen active done

--- screen/main.py
+++ screen/main.py
@@ -1,25 +1,41 @@
 import subprocess
+#import pyautogui
 import time
 
-def get_idle_time():
+
+def take_screenshot(file_path="screenshot/screenshot.png"):
+    # Wait for a moment to ensure the window is active
+    time.sleep(2)
+
+    # Get the screen resolution
+    screen_width, screen_height = pyautogui.size()
+
+    # capture the entire screen
+    screenshot = pyautogui.screenshot()
+
+    # Save the screenshot to the specified file path
+    screenshot.save(file_path)
+    print(f"Screenshot saved to {file_path}")
+
+def is_screen_active():
     try:
-        # Run the xprintidle command and capture the output
-        output = subprocess.check_output(['xprintidle']).decode('utf-8').strip()
-        # Convert the output to seconds
-        idle_time_ms = int(output)
-        idle_time_sec = idle_time_ms / 1000.0
-        return idle_time_sec
+        # Run loginctl to get user session status
+        loginctl_output = subprocess.check_output(['loginctl', 'user-status']).decode('utf-8')
+        
+        # Check if there is an active session
+        return 'State: active' in loginctl_output
+
     except subprocess.CalledProcessError as e:
-        print(f"Error running xprintidle: {e}")
+        print(f"Error running loginctl: {e}")
         return None
 
 # Example usage
 while True:
-    idle_time = get_idle_time()
-    if idle_time is not None:
-        print(f"Idle time: {idle_time} seconds")
-        if idle_time < 300:
-            print("Screen is active")
+    screen_active = is_screen_active()
+    if screen_active is not None:
+        if screen_active:
+            print("Screen is active, taking screen shot...")
+            #take_screenshot()
         else:
             print("Screen is idle")
     time.sleep(5)
